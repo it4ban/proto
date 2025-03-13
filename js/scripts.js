097b7c6b07bb2485cd82ac9d8c756dc53fee634b
Alex Yagodkin <adamsHillzzz@gmail.com>
2025-03-13 12:40:55 +0300

    feat: update structure form and add password hidden/active

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,61 @@
 $(() => {
-	const changeFormBtn = $('button[data-set-form]');
-	const fieldsForm = $('[data-type]');
-	const setPageBtn = $('button[data-set-page]');
+	const userForms = $('.user-form');
+	const formBackIcons = $('.form-backicon');
+	const formButtons = $('.form-button');
+	const formSubbuttons = $('.form-subbutton');
+	const eyeButtons = $('.eye-button');
 
-	console.log(fieldsForm);
-	console.log(setPageBtn);
+	class Validation {}
 
-	function changeFormState(fieldName) {
-		fieldsForm.each(function () {
-			const field = $(this);
+	function handleClick(e) {
+		e.preventDefault();
 
-			if (field.attr('data-type') !== fieldName) {
-				field.fadeOut(400); // Отключаем поле
+		let nextForm = $(e.currentTarget).attr('data-set-form');
+
+		if (nextForm) {
+			userForms.each((_, form) => {
+				let formElem = $(form);
+				let formAttr = formElem.attr('data-form-type');
+				let newHeight = Math.floor(formElem.outerHeight());
+
+				if (formAttr === nextForm) {
+					formElem.animate({ height: `${newHeight}px` }, 400, () => {
+						formElem.css('height', 'auto');
+					});
+
+					formElem.removeClass('user-form--disabled');
+					formElem.addClass('user-form--active');
+				} else {
+					let currentClass = formElem.hasClass('user-form--disabled');
+					if (!currentClass) {
+						formElem.addClass('user-form--disabled');
+						formElem.removeClass('user-form--active');
+					}
+				}
+			});
+		}
+	}
+
+	function handleVisiblePassword(e) {
+		e.preventDefault();
+
+		let parentEye = $(e.currentTarget).closest('.input-password');
+		let eyesButtons = parentEye.find('.eye-button');
+		let inputField = parentEye.find('.form-input');
+
+		eyesButtons.each((_, eye) => {
+			if (!$(eye).hasClass('eye-button--active')) {
+				$(inputField).attr('type', 'text');
+				$(eye).addClass('eye-button--active');
 			} else {
-				field.fadeIn(400); // Включаем поле, если оно соответствует fieldName
+				$(inputField).attr('type', 'password');
+				$(eye).removeClass('eye-button--active');
 			}
 		});
 	}
 
-	changeFormState('auth');
-
-	function handleClick(e) {
-		e.preventDefault();
-		let dataValue = $(e.currentTarget).attr('data-set-form');
-
-		console.log(dataValue);
-		changeFormState(dataValue);
-	}
-
-	changeFormBtn.each((_, btn) => {
-		$(btn).on('click', handleClick);
-	});
+	$(formButtons).on('click', (e) => handleClick(e));
+	$(formSubbuttons).on('click', (e) => handleClick(e));
+	$(formBackIcons).on('click', (e) => handleClick(e));
+	$(eyeButtons).on('click', (e) => handleVisiblePassword(e));
 });
